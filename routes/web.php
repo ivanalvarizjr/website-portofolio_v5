81c3afee454e452250f9106f218dcf4360dfbf0a
ivan <ivanalvarizjr@gmail.com>
2025-08-05 13:08:20 +0700

    commit kelima

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,3 @@ Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/projek', [PageController::class, 'projek'])->name('projek');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-
-// Form Kontak (POST)
-Route::post('/send-message', [PageController::class, 'sendMessage'])->name('send.message');
-
-// Admin Page (optional)
-Route::get('/admin', [PageController::class, 'admin'])->name('admin.dashboard');
-
